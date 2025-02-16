b3b0023cfb9dcbfe830a81966a4f3107f79cc096
ozimmortal <zaleambo@gmail.com>
2025-02-16 19:24:44 +0000

    initialized project

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const socketio = require('socket.io');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://zaleambo:<oliy2627>@freecode.nyvmf3r.mongodb.net/?retryWrites=true&w=majority&appName=freecode";
+
+app.use(express.json());
+app.use(express.static('views'));
+app.use(express.static('public'));
+app.use(session({
+    secret:'1234567890',
+    resave:false,
+    saveUninitialized:false
+}))
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+app.get('/game',(req,res)=>{
+    res.sendFile(__dirname+'/views/game.html')
+})
+let server = app.listen(PORT,()=>{
+    console.log(`listening on http://localhost:${PORT}`);
+})
+
+let io = socketio(server,{});
+
+io.on('connect',(socket)=>{
+    console.log(socket.id,"data")
+})
